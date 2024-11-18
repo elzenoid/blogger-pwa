605be2b63fef6b371152808338efdc1bdc51e653
elzeno.id <143878612+elzenoid@users.noreply.github.com>
2024-11-18 13:15:18 +0700

    Update pwa.config.ts

--- pwa.config.ts
+++ pwa.config.ts
@@ -3,9 +3,9 @@ import type { Config } from './types';
 export default {
   version: '1.0',
   id: '/',
-  name: 'My Blog',
-  shortName: 'My Blog',
-  description: 'My blog description goes here...',
+  name: 'elzeno.id',
+  shortName: 'elzeno.id',
+  description: 'Biografi, Motifasi, Tradisi dan Inspirasi',
   direction: 'auto',
   language: 'en-US',
   backgroundColor: '#fff',
@@ -18,16 +18,16 @@ export default {
   preferRelatedApplications: false,
   shortcuts: [
     {
-      name: 'Shortcut 1',
-      shortName: 'Shortcut 1',
-      description: 'Shortcut 1 description goes here...',
-      url: '/search/label/shortcut-1?utm_source=homescreen',
+      name: 'scr1',
+      shortName: 'scr1',
+      description: 'Gabung Grup WhatsApp Penjaga Tradisi Sunnah',
+      url: '/search/label/scr1?utm_source=homescreen',
     },
     {
-      name: 'Shortcut 2',
-      shortName: 'Shortcut 2',
-      description: 'Shortcut 2 description goes here...',
-      url: '/search/label/shortcut-2?utm_source=homescreen',
+      name: 'scr2',
+      shortName: 'scr2',
+      description: 'Pilih menu favorit di pojok kanan atas',
+      url: '/search/label/scr2?utm_source=homescreen',
     },
   ],
   pwa: {
@@ -40,5 +40,5 @@ export default {
     },
   },
   // Please replace with your blog url if you are using CDN (JsDelivr)
-  origin: 'https://hello-example.blogspot.com',
+  origin: 'https://www.elzeno.id',
 } satisfies Config;
